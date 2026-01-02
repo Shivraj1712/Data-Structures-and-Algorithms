d9368527b198615b5209664250a2716816a0d0b0
Shivraj Maharaul <shivrajmaharaul688@gmail.com>
2026-01-02 15:10:18 +0000

    Create the node and linked list classes with functions

---
+++ code.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Node {
+public :
+    int val ;
+    Node * next ;
+    Node(int val){
+        this -> val = val ;
+        this -> next = NULL ;
+    };
+    Node(){
+        this -> val = NULL ;
+        this -> next = NULL ;
+    };
+    ~Node(){
+        delete next ;
+    } ;
+};
+
+class Linkedlist {
+private :
+    Node * head ;
+    Node * tail ;
+public :
+    Linkedlist(){
+        head = tail = NULL ;
+    };
+    void pushfront(int val){
+        Node * newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode ;
+            return ;
+        }
+        newNode->next = head ;
+        head = newNode ;
+    };
+    void pushback(int val){
+        Node * newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode ;
+            return ;
+        }
+        tail->next = newNode ;
+        tail = newNode ;
+    };
+    void insert(int pos , int val){
+        if(pos < 1){
+            cout<<"Invalid Value"<<endl;
+            return ;
+        }
+        if(pos == 0){
+            pushfront(val);
+            return ;
+        }
+        Node * newNode = new Node(val);
+        if(head == NULL){
+            head = tail = newNode ;
+            return ;
+        }
+        Node * temp = head ;
+        for(int i = 0 ; i < pos - 1 ; i++){
+            temp = temp->next ;
+        }
+        if(temp == NULL){
+            cout<<"Position out of range"<<endl;
+            delete newNode ;
+            return ;
+        }
+        newNode->next = temp->next ;
+        temp->next = newNode ;
+        if(newNode->next == NULL){
+            tail = newNode ;
+        }
+    };
+
+    int search(int target){
+        Node * temp = head ;
+        int index = 0 ;
+        while(temp != NULL){
+            if(temp->val == target) return index ;
+            index++;
+            temp = temp->next ;
+        }
+        return -1 ;
+    };
+    void pop_front(){
+        if(head == NULL){
+            cout<<"List is empty"<<endl;
+            return ;
+        }
+        Node * temp = head ;
+        head = head -> next ;
+        if(head == NULL) tail = NULL ;
+        delete temp ;
+    };
+    void pop_back(){
+        if(head == NULL){
+            cout<<"List is empty"<<endl;
+            return ;
+        }
+        if(head == tail){
+            delete head ;
+            head = tail = NULL ;
+            return ;
+        }
+        Node * temp = head ;
+        while(temp->next != tail){
+            temp = temp->next ;
+        }
+        delete tail ;
+        tail = temp ;
+        tail->next = NULL ;
+    };
+    void printLL(){
+        if(head == NULL){
+            cout<<"List is empty"<<endl;
+            return ;
+        }
+        Node * temp = head ;
+        while(temp != NULL){
+            cout<<temp->val<<" -> ";
+            temp = temp->next ;
+        }
+        cout<<" NULL "<<endl;
+    };
+    ~Linkedlist(){
+        while(head != NULL){
+            pop_front();
+        }
+    };
+};
+
+int main(){
+    return 0;
+}
