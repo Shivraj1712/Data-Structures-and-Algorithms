0ade0179a238b770b2baa4743260ccacd694bdef
Shivraj1712 <shivrajbuilds@outlook.com>
2026-02-10 18:54:49 +0530

    Adding the function of height of binary tree

--- binarytree.cpp
+++ binarytree.cpp
@@ -92,6 +92,12 @@ public:
             }
         }
     }
+    int height(Node * root){
+        if(root==NULL) return 0 ;
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+        return max(leftHeight,rightHeight) + 1 ;
+    }
 
 };
 
