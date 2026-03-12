f4f85590e9777f1803e3aa821529cbc055238d45
Shivraj1712 <shivrajmaharaul688@gmail.com>
2026-03-12 22:41:26 +0530

    Made changes in binarytree.cpp like buildTree function and also added a new class for Binary Search Tree

--- binarytree.cpp
+++ binarytree.cpp
@@ -1,104 +1,113 @@
-#include <iostream>
-#include <bits/stdc++.h>
-using namespace std;
+    #include<iostream>
+    #include<bits/stdc++.h>
+    using namespace std;
 
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-
-    Node(int val) {
-        this->val = val;
-        this->left = NULL;
-        this->right = NULL;
-    }
-};
-
-class BinaryTree {
-private:
-    static int idx = -1;
-
-public:
-    BinaryTree() {
-        this->idx = -1;
-    }
-
-    Node* BuildTree(vector<int>& preorder) {
-        idx++;
-        if (idx >= preorder.size() || preorder[idx] == -1) {
-            return NULL;
+    class Node{
+    public:
+        int val ;
+        Node * left , * right ;
+        Node(int val){
+            this->val = val;
+            this->left = NULL ;
+            this->right = NULL ;
         }
+    };
 
-        Node* root = new Node(preorder[idx]);
-        root->left = BuildTree(preorder);
-        root->right = BuildTree(preorder);
-
-        return root;
-    }
-
-    void preOrderTraversal(Node* root) {
-        if (root == NULL) {
-            return;
+    class BinaryTree{
+    public:
+        static int idx ;
+        Node * root ;
+        BinaryTree(){
+            this->root=NULL ;
+            this->idx = 0 ;
         }
-        cout << root->val << endl;
-        preOrderTraversal(root->left);
-        preOrderTraversal(root->right);
-    }
-
-    void inOrderTraversal(Node* root) {
-        if (root == NULL) {
-            return;
+        Node * buildTree(vector<int>&arr){
+            if(idx >= arr.size() || arr[idx] == -1){
+                idx++;
+                return NULL ;
+            }
+            Node * root = new Node(arr[idx++]);
+            root->left = buildTree(arr);
+            root->right = buildTree(arr);
+            return root ;
         }
-        inOrderTraversal(root->left);
-        cout << root->val << endl;
-        inOrderTraversal(root->right);
-    }
-
-    void postOrderTraversal(Node* root) {
-        if (root == NULL) {
-            return;
-        }
-        postOrderTraversal(root->left);
-        postOrderTraversal(root->right);
-        cout << root->val << endl;
-    }
-
-    void levelOrderTraversal(Node * root){
-        if(root == NULL){
-            return ;
-        }
-        queue<Node*> q ;
-        q.push(root);
-        q.push(NULL);
-        while(!q.empty()){
-            Node * curr = q.front() ;
-            q.pop();
-            if(curr == NULL){
-                cout<<endl ;
-                if(q.empty()){
-                    break ;
+        vector<vector<int>> levelOrderTraversal(){
+            if(this->root == NULL) return {};
+            queue<Node*> q ;
+            vector<vector<int>> traversal;
+            vector<int> values ;
+            q.push(this->root);
+            q.push(NULL);
+            while(!q.empty()){
+                Node * curr = q.front();
+                q.pop();
+                if(curr == NULL){
+                    traversal.push(values);
+                    if(q.empty()) break ;
+                    else{
+                        q.push(NULL);
+                        values.clear();
+                    }
                 }else{
-                    q.push(NULL);
-                }
-            }else{
-                cout<<curr->val<<" ";
-                if(curr->left != NULL){
-                    q.push(curr->left);
-                }
-                if(curr->right != NULL){
-                    q.push(curr->right);
+                    values.push_back(curr->val);
+                    if(curr->left != NULL) q.push(curr->left);
+                    if(curr->right != NULL) q.push(curr->right);
                 }
             }
+            return traversal;
+        };
+        void preOrderTraversal(Node * root){
+            if(!root) return ;
+            cout<<root->val<<" ";
+            preOrderTraversal(root->left);
+            preOrderTraversal(root->right);
         }
-    }
-    int height(Node * root){
-        if(root==NULL) return 0 ;
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
-        return max(leftHeight,rightHeight) + 1 ;
-    }
+        void inOrderTraversal(Node * root){
+            if(!root) return ;
+            inOrderTraversal(root->left);
+            cout<<root->val<<" ";
+            inOrderTraversal(root->right);
+        }
+        void postOrderTraversal(Node * root){
+            if(!root) return ;
+            postOrderTraversal(root->left);
+            postOrderTraversal(root->right);
+            cout<<root->val<<" ";
+        }
+    };
 
-};
+    int BinaryTree::idx = 0 ;
 
-int BinaryTree::idx = -1 ;
+    class BinarySearchTree{
+    public: 
+        static int idx ;
+        Node * root ;
+        BinarySearchTree(){
+            this->root = NULL ;
+            this->idx = 0 ;
+        }
+        Node * insert(Node * root,int val){
+            if(!root){
+                Node * newNode = new Node(val);
+                root = newNode ;
+                return root ;
+            }else{
+                if(root->val >= val){
+                    root->left = insert(root->left, val);
+                }else{
+                    root->right = insert(root->right,val);
+                }
+                return root ;
+            }
+        }
+        Node * buildBST(vector<int>& arr,Node * root){
+            if(arr.size() == 0) return NULL ;
+            for(int i : arr){
+                root = insert(root,i);
+            }
+            this->root = root ;
+            return root ;
+        }
+    };
+
+    int BinarySearchTree::idx = 0 ;
